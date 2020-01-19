b867e690a5a950e150159241aec11f7ed2d4c98e
Phil Mass <philrmass@gmail.com>
2020-01-19 14:50:37 -0800

    set offset in interval

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Header from './Header';
 import Meals from './Meals';
 import Picker from './Picker';
 import styles from '../styles/App.module.css';
 
 function App() {
+  const mainRef = useRef(null);
   const [showMeals, setShowMeals] = useState(false);
+
+  useEffect(() => {
+    setInterval(() => {
+      const sec = Math.round((Date.now() / 1000) % 60);
+      mainRef.current.style.setProperty('--page-offset', `${30 - sec}px`);
+    }, 1000);
+  }, []);
 
   return (
     <div className={styles.page}>
       <Header
         showMeals={setShowMeals}
       />
-      <div className={styles.main}>
+      <div className={styles.main} ref={mainRef}>
         <div className={styles.left}>
         </div>
         <div className={styles.content}>

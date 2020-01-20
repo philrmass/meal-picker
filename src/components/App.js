0c839411237e394458afdebc34dd004979cbbe38
Phil Mass <philrmass@gmail.com>
2020-01-19 16:36:27 -0800

    add swipe animationand change

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import Header from './Header';
 import Meals from './Meals';
 import Picker from './Picker';
@@ -7,20 +7,65 @@ import styles from '../styles/App.module.css';
 function App() {
   const mainRef = useRef(null);
   const [showMeals, setShowMeals] = useState(false);
+  const [startX, setStartX] = useState(null);
 
-  useEffect(() => {
-    setInterval(() => {
-      const sec = Math.round((Date.now() / 1000) % 60);
-      mainRef.current.style.setProperty('--page-offset', `${30 - sec}px`);
-    }, 1000);
-  }, []);
+  function handleStart(event) {
+    const x = getX(event);
+    setStartX(x);
+  }
+
+  function handleMove(event) {
+    handleSwipe(getX(event) - startX);
+  }
+
+  function handleEnd() {
+    setOffset(0);
+  }
+
+  function handleSwipe(delta) {
+    const deadZone = 20;
+    const swipeChange = 140;
+    const absDelta = Math.abs(delta);
+    const swipe = absDelta > deadZone ? absDelta : 0;
+    const isRight = delta > 0;
+    if (isRight && !showMeals) {
+      setOffset(swipe);
+      if (swipe > swipeChange) {
+        setShowMeals(true);
+        setOffset(0);
+      }
+    }
+    if (!isRight && showMeals) {
+      setOffset(-swipe);
+      if (swipe > swipeChange) {
+        setShowMeals(false);
+        setOffset(0);
+      }
+    }
+  }
+
+  function getX(event) {
+    return event.touches[0].clientX;
+  }
+
+  function setOffset(swipe) {
+    const scaled = 0.3 * swipe;
+    const clipped = Math.max(-30, Math.min(30, scaled));
+    mainRef.current.style.setProperty('--page-offset', `${Math.round(clipped)}px`);
+  }
 
   return (
     <div className={styles.page}>
       <Header
         showMeals={setShowMeals}
       />
-      <div className={styles.main} ref={mainRef}>
+      <div 
+        ref={mainRef}
+        className={styles.main}
+        onTouchStart={handleStart}
+        onTouchMove={handleMove}
+        onTouchEnd={handleEnd}
+      >
         <div className={styles.left}>
         </div>
         <div className={styles.content}>
